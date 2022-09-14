fd85256c7b35e5ed3dd2d6cb98b6fc4e925f1916
AndreyBoz <bozhov.a@yandex.kz>
2022-09-15 01:58:37 +0300

    Add project files.

---
+++ Linked List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Linked_List {
+    class Program {
+        static void Main(string[] args) {
+            var list = new Model.LinkedList<int>();
+            
+            list.Add(2);
+            list.Add(3);
+            list.Add(1);
+
+            foreach (var item in list) {
+                Console.Write(item + " ");    
+            }
+            list.Delete(1);
+            Console.WriteLine(list.Count);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
